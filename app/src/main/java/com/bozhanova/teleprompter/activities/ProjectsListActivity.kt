2890b6b318413da17fcfb2ffa355f50807e3ce58
TamaraBozhanova <tamarab@wizardsdev.com>
2020-12-26 21:42:48 +0200

    deleted unnesesary code

--- app/src/main/java/com/bozhanova/teleprompter/activities/ProjectsListActivity.kt
+++ app/src/main/java/com/bozhanova/teleprompter/activities/ProjectsListActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -23,57 +22,26 @@ class ProjectsListActivity : AppCompatActivity(), ProjectsFragment.OnItemClicked
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_projectslist)
         setupBottomNavMenu(findNavController(R.id.fragmentNavHost))
-        //val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-      //  bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-      //  openFragment(ProjectsFragment.newInstance())
         projects_fab.setOnClickListener {
             onFabClick()
         }
     }
-
-//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//        when (item.itemId) {
-//            R.id.menuHome -> {
-//                val projectsFragment = ProjectsFragment.newInstance()
-//                openFragment(projectsFragment)
-//                projects_fab.setImageResource(R.drawable.add_white)
-//                fabPlayIcon = false;
-//                true
-//            }
-//            R.id.menuSettings -> {
-//                val settingsFragment = SettingsFragment.newInstance()
-//                openFragment(settingsFragment)
-//                projects_fab.setImageResource(R.drawable.add_white)
-//                fabPlayIcon = false;
-//                true
-//            }
-//        }
-//        false
-//    }
 
     private fun setupBottomNavMenu(navController: NavController) {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav?.setupWithNavController(navController)
     }
 
-    private fun openFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
 
     private fun onFabClick(){
         if(fabPlayIcon){
-//            val i = Intent(this, MainActivity::class.java)
-//            i.putExtra("scenario", scenario)
-//            startActivity(i)
             bottomAppBar.visibility = View.GONE;
             projects_fab.hide();
             val bundle = bundleOf("project_scenario" to scenario)
             this.findNavController(R.id.fragmentNavHost).navigate(R.id.action_detail_projects_fragment_des_to_videoFragment, bundle)
         }
     }
+
     override fun onItemSelected(scenario : String) {
         projects_fab.setImageResource(R.drawable.play_arrow_36)
         fabPlayIcon = true;
